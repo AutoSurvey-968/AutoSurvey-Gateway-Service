49da64fedad820df34636f4c5a83002cad58147e
tyler lopez <tylerlopezccu@gmail.com>
2021-05-26 17:24:25 -0400

    Changed error mapping to return a Mono<ResponseEntity<String>>

--- src/main/java/com/revature/autosurvey/gateway/ErrorController.java
+++ src/main/java/com/revature/autosurvey/gateway/ErrorController.java
@@ -4,12 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import reactor.core.publisher.Mono;
+
 @Controller
 public class ErrorController {
 
   @GetMapping(value="error")
-  public ResponseEntity<String> handleError() {
+  public Mono<ResponseEntity<String>> handleError() {
 
-    return ResponseEntity.status(500).body("Service is currently down, please try again in a few minutes.");
+    return Mono.just(ResponseEntity.status(500).body("Service is currently down, please try again in a few minutes."));
   }
 }
