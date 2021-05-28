ab295a6910ff24e225592ee02744e75a2a2bf756
DuskDaleSpider <dakota.ray.clark@gmail.com>
2021-05-28 16:25:12 -0400

    fixed run-docker.sh

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Check if autosurvey-network network exists
-if [ -z $(docker network ls -q -f name=autosurvey-network) ]; then
+if [ -z "$(docker network ls -q -f name=autosurvey-network)" ]; then
     docker network create autosurvey-network
 fi
 
 # rm gateway-service container if it exists
-if [ -n $(docker container ls -aqf name=gateway-service) ]; then
+if [ -n "$(docker container ls -aqf name=gateway-service)" ]; then
     docker container stop gateway-service
     docker container rm gateway-service
 fi
