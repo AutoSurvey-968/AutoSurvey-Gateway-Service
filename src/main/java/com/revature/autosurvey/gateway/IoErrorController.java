b5b3f3039f95aeb99ef9a287d26af6d04a97d48e
mcinerneym <mcinerney.michael.t@gmail.com>
2021-09-02 11:41:20 -0700

    Fixed formatting error

--- src/main/java/com/revature/autosurvey/gateway/IoErrorController.java
+++ src/main/java/com/revature/autosurvey/gateway/IoErrorController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import reactor.core.publisher.Mono;
+
 @RestController
 @RequestMapping("ioerror")
 
 public class IoErrorController {
-@PostMapping
-	  public Mono<ResponseEntity<Object>> handlePost() {
-	    return Mono.just(ResponseEntity.status(503).build());
-	  }
+	@PostMapping
+	public Mono<ResponseEntity<Object>> handlePost() {
+		return Mono.just(ResponseEntity.status(503).build());
+	}
 }
