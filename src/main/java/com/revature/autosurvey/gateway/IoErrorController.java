d251482967de7305e1aadf9b0baea1d76eb0f034
elizabethkelvin <elizabethkelvin90@gmail.com>
2021-09-02 11:35:43 -0700

    minorchange

--- src/main/java/com/revature/autosurvey/gateway/IoErrorController.java
+++ src/main/java/com/revature/autosurvey/gateway/IoErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
 @RestController
-@RequestMapping("email/ioerror")
+@RequestMapping("ioerror")
 
 public class IoErrorController {
 @PostMapping
