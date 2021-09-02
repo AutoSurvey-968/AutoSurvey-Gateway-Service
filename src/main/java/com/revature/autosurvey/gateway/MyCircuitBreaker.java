8e2e4654fb2d28739cc1649fd3d257633e5cb7ac
mcinerneym <mcinerney.michael.t@gmail.com>
2021-09-02 14:24:24 -0700

    Ideas

--- src/main/java/com/revature/autosurvey/gateway/MyCircuitBreaker.java
+++ src/main/java/com/revature/autosurvey/gateway/MyCircuitBreaker.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
@@ -17,7 +18,17 @@ public class MyCircuitBreaker {
   @Bean
   public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
     return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-            .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+            .circuitBreakerConfig(CircuitBreakerConfig.custom()
+            		.failureRateThreshold(50)
+            		  .slowCallRateThreshold(50)
+            		  .waitDurationInOpenState(Duration.ofMillis(1000))
+            		  .slowCallDurationThreshold(Duration.ofSeconds(2))
+            		  .permittedNumberOfCallsInHalfOpenState(3)
+            		  .minimumNumberOfCalls(10)
+            		  .slidingWindowType(SlidingWindowType.TIME_BASED)
+            		  .slidingWindowSize(5)
+            		  .recordExceptions(Throwable.class)
+            		  .build())
             .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
   }
 }
