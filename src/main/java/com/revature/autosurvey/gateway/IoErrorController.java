12596bc9a0d43408f0dfffbda85adb393a8b09d2
elizabethkelvin <elizabethkelvin90@gmail.com>
2021-09-02 11:00:16 -0700

    IoErrorController

---
+++ src/main/java/com/revature/autosurvey/gateway/IoErrorController.java
@@ -0,0 +1,18 @@
+package com.revature.autosurvey.gateway;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import reactor.core.publisher.Mono;
+@RestController
+@RequestMapping("email/ioerror")
+
+public class IoErrorController {
+@PostMapping
+	  public Mono<ResponseEntity<Object>> handlePost() {
+	    return Mono.just(ResponseEntity.status(503).build());
+	  }
+}
